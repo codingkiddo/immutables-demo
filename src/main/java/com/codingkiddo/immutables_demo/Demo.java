cf2105b9f9ac318062b24a2750b8fa58b0e85586
codingkiddo <codingkiddo@gmail.com>
2025-11-05 10:51:32 +0530

    modified:   pom.xml 	modified:   src/main/java/com/codingkiddo/immutables_demo/Demo.java 	modified:   src/main/java/com/codingkiddo/immutables_demo/Person.java

--- src/main/java/com/codingkiddo/immutables_demo/Demo.java
+++ src/main/java/com/codingkiddo/immutables_demo/Demo.java
@@ -1,11 +1,23 @@
 package com.codingkiddo.immutables_demo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
 public class Demo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JsonProcessingException {
 		Person person = ImmutablePerson.builder().name("CodingKiddo")
 				.age(35).build();
 		
 		System.out.println(person);
 		
+		ObjectMapper objectMapper = new ObjectMapper()
+		        .registerModule(new Jdk8Module()); // safe if using Optional, etc.
+
+		String json = objectMapper.writeValueAsString(person);
+		System.out.println(json);
+		
+		Person roundTripped = objectMapper.readValue(json, ImmutablePerson.class);
+		System.out.println(roundTripped);
 	}
 }
